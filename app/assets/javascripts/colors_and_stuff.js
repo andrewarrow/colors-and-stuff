21c05c214f6561090e34e782fed885bbd0895502
Andrew Arrow <andrew@anybeat.com>
2012-04-16 14:01:30 -0700

    progress

--- app/assets/javascripts/colors_and_stuff.js
+++ app/assets/javascripts/colors_and_stuff.js
@@ -1,4 +1,14 @@
 $(function() {
-  $( "#dialog" ).dialog({ position: [300, 650], resizable: false, title: 'Controls', width: 800, height: 200, 
-                          closeOnEscape: false });
+  //$( "#dialog" ).dialog({ position: [300, 650], resizable: false, title: 'Controls', width: 800, height: 200, 
+  //                        closeOnEscape: false });
 });
+
+
+var Colors = {
+  navbar: function() {
+    $('.navbar-inner').attr('style', 'background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#FFBF00), to(#FDEE00))');
+    $('.navbar .brand').attr('style', 'color: #000');
+    //$('.navbar .nav > li > a').attr('style', 'color: #333; text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);');
+    //background-image  -webkit-gradient(linear, 0 0, 0 100%, from(#111), to(#AAA));
+  }
+};
