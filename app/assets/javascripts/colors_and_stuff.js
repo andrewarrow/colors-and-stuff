ee74294f7a4df16ded690aee48b3dab2fb9e5872
Andrew Arrow <andrew@anybeat.com>
2012-04-16 11:19:12 -0700

    dialog options

--- app/assets/javascripts/colors_and_stuff.js
+++ app/assets/javascripts/colors_and_stuff.js
@@ -1,3 +1,4 @@
 $(function() {
-  $( "#dialog" ).dialog();
+  $( "#dialog" ).dialog({ position: [300, 650], resizable: false, title: 'Controls', width: 800, height: 200, 
+                          closeOnEscape: false });
 });
