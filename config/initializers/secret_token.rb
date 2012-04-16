68288c660a55a2cc2bf3d2d965d6a63e44a8d0cc
Andrew Arrow <andrew@anybeat.com>
2012-04-16 10:46:23 -0700

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColorsAndStuff::Application.config.secret_token = '0c7e77f99baa2dc45ac057d7e5bf66583f673aac54e7d90cb46f3c7f5fa6bb42e95a892740ae0cdb708368157eec352a68953a4e894da222df987fba29bed968'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+ColorsAndStuff::Application.routes.draw do
+  root :to => 'welcome#index'
+end
